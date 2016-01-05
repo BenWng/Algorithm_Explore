d39f23d12eca10686da844cebecdf332193af5b3
Zhongxi Wang <Ben_Big@Big.local>
2016-01-05 02:38:42 -0500

    Finish Q_1_1

---
+++ src/Cracking_Interview/Ch1_Arrays_And_String/Q_1_1.java
@@ -0,0 +1,7 @@
+package Cracking_Interview.Ch1_Arrays_And_String;
+
+/**
+ * Created by Ben_Big on 1/5/16.
+ */
+public class Q_1_1 {
+}
---
+++ src/Cracking_Interview/Ch2_Linked_List/Q_2_1.java
@@ -0,0 +1,7 @@
+package Cracking_Interview.Ch2_Linked_List;
+
+/**
+ * Created by Ben_Big on 1/5/16.
+ */
+public class Q_2_1 {
+}
