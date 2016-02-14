fe89333ff98e65d5fe5d8cf5a53e3b75401234e6
BenWng <wnghan@gmail.com>
2016-02-14 14:24:57 -0500

    Fix a problem in ClimbingStairs

--- src/LeetCode/ClimbingStairs.java
+++ src/LeetCode/ClimbingStairs.java
@@ -7,19 +7,19 @@ public class ClimbingStairs {
     public static int climbStairs(int n){
         int [] Ary=new int[n];
         for (int i=0;i<n;i++){
-            if (n==0){  //n==0 corresponds to n==1
-                Ary[n]=1;
+            if (i==0){  //n==0 corresponds to n==1
+                Ary[i]=1;
             }
-            else if (n==1){
-                Ary[n]=2;
+            else if (i==1){
+                Ary[i]=2;
             }
             else{
-                Ary[n]=2+Ary[n-1]+Ary[n-2];
+                Ary[i]=Ary[i-1]+Ary[i-2];
             }
         }
         return Ary[n-1];
     }
     public static void main(String[] args){
-
+        System.out.println(climbStairs(18));
     }
 }
