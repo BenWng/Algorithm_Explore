5ccb08fab2e24d105f674065355c60ad6d9e812e
BenWng <wnghan@gmail.com>
2016-02-14 14:12:00 -0500

    Update ClimbingStairs

--- src/LeetCode/ClimbingStairs.java
+++ src/LeetCode/ClimbingStairs.java
@@ -4,4 +4,22 @@ package LeetCode;
  * Created by Ben_Big on 2/14/16.
  */
 public class ClimbingStairs {
+    public static int climbStairs(int n){
+        int [] Ary=new int[n];
+        for (int i=0;i<n;i++){
+            if (n==0){  //n==0 corresponds to n==1
+                Ary[n]=1;
+            }
+            else if (n==1){
+                Ary[n]=2;
+            }
+            else{
+                Ary[n]=2+Ary[n-1]+Ary[n-2];
+            }
+        }
+        return Ary[n-1];
+    }
+    public static void main(String[] args){
+
+    }
 }
