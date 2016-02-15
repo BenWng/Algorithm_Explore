3e71bc7b3cca68652c1b8391730e84f346634bc8
BenWng <wnghan@gmail.com>
2016-02-15 11:08:57 -0500

    Initiate NumArray

---
+++ src/LeetCode/NumArray.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+/**
+ * Created by ben on 2/15/16.
+ */
+
+
+/**
+ * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+ Example:
+ Given nums = [-2, 0, 3, -5, 2, -1]
+
+ sumRange(0, 2) -> 1
+ sumRange(2, 5) -> -1
+ sumRange(0, 5) -> -3
+ Note:
+ You may assume that the array does not change.
+ There are many calls to sumRange function.
+ */
+public class NumArray {
+    public NumArray(int[] nums) {
+
+    }
+
+    public int sumRange(int i, int j) {
+
+    }
+
+
+}
