c6420bc98fad0354771fc1c5f578b27890b562cc
BenWng <wnghan@gmail.com>
2016-02-16 14:02:42 -0500

    Initialize WordBreak

---
+++ src/LeetCode/WordBreak.java
@@ -0,0 +1,24 @@
+package LeetCode;
+import java.util.*;
+
+/**
+ * Created by ben on 2/16/16.
+ */
+
+/**
+ * Given a string s and a dictionary of words dict,
+ * determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+ For example, given
+ s = "leetcode",
+ dict = ["leet", "code"].
+
+ Return true because "leetcode" can be segmented as "leet code".
+ */
+
+
+public class  WordBreak{
+    public boolean wordBreak(String s, Set<String> wordDict) {
+
+    }
+}
