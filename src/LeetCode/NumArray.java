6262b2e4928f99a332aead253d3efbc429f0ee31
BenWng <wnghan@gmail.com>
2016-02-17 21:49:32 -0500

    Finish NumArray

--- src/LeetCode/NumArray.java
+++ src/LeetCode/NumArray.java
@@ -6,7 +6,8 @@ package LeetCode;
 
 
 /**
- * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+ * Given an integer array nums,
+ * find the sum of the elements between indices i and j (i ≤ j), inclusive.
 
  Example:
  Given nums = [-2, 0, 3, -5, 2, -1]
@@ -18,14 +19,44 @@ package LeetCode;
  You may assume that the array does not change.
  There are many calls to sumRange function.
  */
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray = new NumArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.sumRange(1, 2);
+
 public class NumArray {
+
+    public int [] Ary;
+
+
     public NumArray(int[] nums) {
-
+        if(nums.length>1){
+            Ary=new int[nums.length];
+            Ary[0]=nums[0];
+            for (int i=1;i<nums.length;i++){
+                Ary[i]=nums[i]+Ary[i-1];
+            }
+        }
+        else if (nums.length==1){
+            Ary=new int[1];
+            Ary[0]=nums[0];
+        }
     }
-
     public int sumRange(int i, int j) {
-
+        if(i>0){
+            return Ary[j]-Ary[i-1];
+        }
+        else {
+            return Ary[j];
+        }
     }
 
+    public static void main(String[] args){
+        int [] nums={-1};
+        NumArray numArray = new NumArray(nums);
+        System.out.println(numArray.sumRange(0,0));//sumRange(2,5),sumRange(0,5)
 
+    }
 }
