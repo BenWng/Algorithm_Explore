01e5ba7f2bcea0f436c5fe6ec0757387e9b2485e
BenWng <wnghan@gmail.com>
2016-09-30 15:57:28 +0800

    Finish 264 Ugly Number

--- src/LeetCode/UglyNumberII.java
+++ src/LeetCode/UglyNumberII.java
@@ -4,6 +4,7 @@ package LeetCode;
  * Created by Ben_Big on 7/12/16.
  */
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.PriorityQueue;
 
@@ -14,8 +15,11 @@ import java.util.PriorityQueue;
  * Note that 1 is typically treated as an ugly number.
  */
 
-//ToDo: This question needs to be revisited
+//ToDo: this question needs revisit
 
+
+/*
+// A super slow idea
 public class UglyNumberII {
     Integer[] primes = {2, 3, 5};
 
@@ -53,3 +57,120 @@ public class UglyNumberII {
     }
 
 }
+*/
+
+//The following idea is on the right track, but still a little bit slow
+/*
+public class UglyNumberII {
+    int[] primes={2,3,5};
+    long[] multiplyResultCache={2,3,5};
+    ArrayList<Long> uglyNumberList=new ArrayList<>();
+
+    public int nthUglyNumber(int n) {
+        if (n==1){
+            return 1;
+        }
+        int pointerForUglyNumberList=0;
+        uglyNumberList.add((long)1);
+
+        while (uglyNumberList.size()<n){
+            //Add the smallest number in the cache to the uglyNumberList
+            long smallestMultiplyResultSoFar=multiplyResultCache[0];
+            int indexOfSmallestMultiplyResultInCache=0;
+
+            for (int i=0;i<3;i++){
+                if (multiplyResultCache[i]<smallestMultiplyResultSoFar){
+                    smallestMultiplyResultSoFar=multiplyResultCache[i];
+                    indexOfSmallestMultiplyResultInCache=i;
+                }
+            }
+            uglyNumberList.add(smallestMultiplyResultSoFar);
+
+            //Move the pointer
+            if (uglyNumberList.get(pointerForUglyNumberList)*5<=uglyNumberList.get(uglyNumberList.size()-1)) {
+                pointerForUglyNumberList++;
+            }
+
+
+            //Supplant in the cache
+            int numberToMultiply=primes[indexOfSmallestMultiplyResultInCache];
+
+            int temporaryPointer=pointerForUglyNumberList;
+            while(temporaryPointer<uglyNumberList.size()){
+                long multiplyResult=uglyNumberList.get(temporaryPointer)*numberToMultiply;
+                if (multiplyResult>uglyNumberList.get(uglyNumberList.size()-1) &&
+                        multiplyResult!=multiplyResultCache[0] &&
+                        multiplyResult!=multiplyResultCache[1] &&
+                        multiplyResult!=multiplyResultCache[2])
+                    {
+                        multiplyResultCache[indexOfSmallestMultiplyResultInCache]=multiplyResult;
+                        break;
+                    }
+                temporaryPointer++;
+            }
+
+
+        }
+
+        long result= uglyNumberList.get(uglyNumberList.size()-1);
+        return (int) result;
+
+    }
+
+    public static void main(String[] args) {
+        UglyNumberII test=new UglyNumberII();
+
+        System.out.println(test.nthUglyNumber(1690));
+
+    }
+}*/
+
+public class UglyNumberII {
+
+
+    public int nthUglyNumber(int n){
+        if(n==1){
+            return 1;
+        }
+        long[] uglyNumberList=new long[n];
+        uglyNumberList[0]=1l;
+        int numberOfUglyNumber=1;//record how many ugly numbers have been generated
+
+        //At first, all the pointers point to index 0
+        int pointerFor2=0;
+        int pointerFor3=0;
+        int pointerFor5=0;
+
+        while (numberOfUglyNumber<n){
+            long multiplyWith2=uglyNumberList[pointerFor2]*2;
+            long multiplyWith3=uglyNumberList[pointerFor3]*3;
+            long multiplyWith5=uglyNumberList[pointerFor5]*5;
+
+
+            long minimum=multiplyWith2<multiplyWith3?multiplyWith2:multiplyWith3;
+            minimum=minimum<multiplyWith5?minimum:multiplyWith5;
+
+            if(minimum==multiplyWith2){
+                pointerFor2++;
+            }
+            if(minimum==multiplyWith3){
+                pointerFor3++;
+            }
+            if(minimum==multiplyWith5){
+                pointerFor5++;
+            }
+            uglyNumberList[numberOfUglyNumber]=minimum;
+            numberOfUglyNumber++;
+
+        }
+        return (int)uglyNumberList[n-1];
+    }
+
+    public static void main(String[] args) {
+        UglyNumberII test=new UglyNumberII();
+
+        System.out.println(test.nthUglyNumber(1690));
+
+    }
+}
+
