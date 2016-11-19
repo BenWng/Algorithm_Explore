8381c4fa65fcf373448f5139078acfba55f618b8
BenWng <wnghan@gmail.com>
2016-11-20 02:54:15 +0800

    Finish 146 LRU Cache

--- src/LeetCode/LRUCache.java
+++ src/LeetCode/LRUCache.java
@@ -27,16 +27,65 @@ import java.util.LinkedList;
 public class LRUCache {
 
     class DoubleLinkedListNode{
-
+        int val;
+        DoubleLinkedListNode next;
+        DoubleLinkedListNode pre;
+        DoubleLinkedListNode(int val){
+            this.val=val;
+            this.next=null;
+            this.pre=null;
+        }
     }
 
     class DoubleLinkedList{
-
+        DoubleLinkedListNode head;
+        DoubleLinkedListNode tail;
+        int size=0;
+        DoubleLinkedList(){
+            head=new DoubleLinkedListNode(0);
+            tail=new DoubleLinkedListNode(0);
+            head.next=tail;
+            tail.pre=head;
+        }
+        void addToFront(DoubleLinkedListNode node){
+            DoubleLinkedListNode nextNode=head.next;
+            head.next=node;
+            node.pre=head;
+            node.next=nextNode;
+            nextNode.pre=node;
+            size++;
+        }
+        void addToBack(DoubleLinkedListNode node){
+            DoubleLinkedListNode preNode=tail.pre;
+            preNode.next=node;
+            node.pre=preNode;
+            node.next=tail;
+            tail.pre=node;
+            size++;
+        }
+        void remove(DoubleLinkedListNode node){
+            DoubleLinkedListNode preNode=node.pre;
+            DoubleLinkedListNode nextNode=node.next;
+            preNode.next=nextNode;
+            nextNode.pre=preNode;
+            size--;
+        }
+        DoubleLinkedListNode first(){
+            return head.next;
+        }
+        DoubleLinkedListNode last(){
+            return tail.pre;
+        }
+        int size(){
+            return size;
+        }
     }
+
+
     int capacity;
     HashMap<Integer,Integer> keyToValue=new HashMap<>();
-    HashMap<Integer,Integer>keyToIndex=new HashMap<>();
-    LinkedList<Integer> useFrequency=new LinkedList<>();//The head of the linkedlist is least used...
+    HashMap<Integer,DoubleLinkedListNode>keyToIndex=new HashMap<>();
+    DoubleLinkedList useFrequency=new DoubleLinkedList();//The head of the linkedlist is least used...
     //... The tail is just used.
     public LRUCache(int capacity) {
         this.capacity=capacity;
@@ -45,11 +94,9 @@ public class LRUCache {
     public int get(int key) {
         if (!keyToValue.containsKey(key)) return -1;
         int result=keyToValue.get(key);
-        int index=keyToIndex.get(key);
-        useFrequency.remove(index);
-        useFrequency.addLast(key);
-        int newIndex=useFrequency.size()-1;
-        keyToIndex.replace(key,index,newIndex);
+        DoubleLinkedListNode oldNode=keyToIndex.get(key);
+        useFrequency.remove(oldNode);
+        useFrequency.addToBack(oldNode);
         return result;
     }
 
@@ -57,31 +104,32 @@ public class LRUCache {
         if (keyToValue.containsKey(key)){
             int oldValue=keyToValue.get(key);
             keyToValue.replace(key,oldValue,value);
-            int index=keyToIndex.get(key);
-            useFrequency.remove(index);
-            useFrequency.addLast(key);
-            int newIndex=useFrequency.size()-1;
-            keyToIndex.replace(key,index,newIndex);
+            DoubleLinkedListNode oldNode=keyToIndex.get(key);
+            useFrequency.remove(oldNode);
+            useFrequency.addToBack(oldNode);
         }
         else{
             if (useFrequency.size()==capacity) {
-                int oldKey = useFrequency.getFirst();
-                useFrequency.remove(0);
+                DoubleLinkedListNode first= useFrequency.first();
+                useFrequency.remove(first);
+                int oldKey=first.val;
                 keyToValue.remove(oldKey);
                 keyToIndex.remove(oldKey);
             }
             keyToValue.put(key,value);
-            useFrequency.addLast(key);
-            int newIndex=useFrequency.size()-1;
-            keyToIndex.put(key,newIndex);
+            DoubleLinkedListNode newNode=new DoubleLinkedListNode(key);
+            useFrequency.addToBack(newNode);
+            keyToIndex.put(key,newNode);
         }
     }
     public static void main(String[] args){
         LRUCache lruc=new LRUCache(3);
         System.out.println(lruc.get(3));//expect -1
-        lruc.set(5,8);
-        lruc.set(6,7);
+        lruc.set(5,-8);
+        lruc.set(6,-7);
         System.out.println(lruc.get(5));
+        lruc.set(18,-21);
+        lruc.set(33,-35);
     }
 
 
