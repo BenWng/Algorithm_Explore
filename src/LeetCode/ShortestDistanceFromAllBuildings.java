e93ebb738a0b2819192d0965743cacb9604448c5
BenWng <wnghan@gmail.com>
2016-11-27 03:20:29 +0800

    Finish 317 Shortest Distance From All Buildings

--- src/LeetCode/ShortestDistanceFromAllBuildings.java
+++ src/LeetCode/ShortestDistanceFromAllBuildings.java
@@ -33,9 +33,10 @@ import java.util.*;
 
 
 public class ShortestDistanceFromAllBuildings {
-    private int[][][] sumDistanceToHouses;
+    private int[][] sumDistanceToHouses;
+    private boolean houseNotReachable=false;
     public int shortestDistance(int[][] grid) {
-        sumDistanceToHouses=new int[grid.length][][];
+        sumDistanceToHouses=new int[grid.length][];
         HashSet<Map.Entry<Integer,Integer>> housePositions=new HashSet<>();
         for (int i=0;i<grid.length;i++){
             for (int j=0;j<grid[0].length;j++){
@@ -47,30 +48,18 @@ public class ShortestDistanceFromAllBuildings {
         }
         int numHouses=housePositions.size();
         for (int i=0;i<grid.length;i++){
-            sumDistanceToHouses[i]=new int [grid[0].length][];
-            for (int j=0;j<grid[0].length;j++){
-                sumDistanceToHouses[i][j]=new int[numHouses];
-            }
+            sumDistanceToHouses[i]=new int [grid[0].length];
         }
         int houseNumber=0;
         for (Map.Entry<Integer,Integer>house : housePositions) {
             BFS(house, grid,houseNumber);
             houseNumber++;
+            if (houseNotReachable) return -1;
         }
         int result=Integer.MAX_VALUE;
         for (int i=0;i<sumDistanceToHouses.length;i++){
             for (int j=0;j<sumDistanceToHouses[0].length;j++){
-                int currentSumDistance=0;
-                for (int k=0;k<numHouses;k++) {
-                    if (sumDistanceToHouses[i][j][k] == 0 ){
-                        currentSumDistance=0;
-                        break;
-                    }
-                    else{
-                        currentSumDistance+=sumDistanceToHouses[i][j][k];
-                    }
-                }
-                if (currentSumDistance!=0 && currentSumDistance<result) result=currentSumDistance;
+                if (sumDistanceToHouses[i][j]!=0 && sumDistanceToHouses[i][j]<result) result=sumDistanceToHouses[i][j];
             }
         }
         if (result==Integer.MAX_VALUE){
@@ -86,62 +75,76 @@ public class ShortestDistanceFromAllBuildings {
         }
         Queue<Map.Entry<Integer,Integer>> queue=new LinkedList<>();
         queue.add(startPos);
-        HashSet<Map.Entry<Integer,Integer>> visitedPlaces=new HashSet<>();
-        visitedPlaces.add(startPos);
+        int numReachablePoints=0;
         while(!queue.isEmpty()){
             Map.Entry<Integer,Integer> position=queue.poll();
             int x=position.getKey();
             int y=position.getValue();
-            if (x+1<grid.length && grid[x+1][y]==0){
+            if (x+1<grid.length && grid[x+1][y]==0 && distanceToThisHouse[x+1][y]==0){
                 Map.Entry<Integer,Integer>currentPos=new AbstractMap.SimpleEntry(x+1,y);
-                if (!visitedPlaces.contains(currentPos)){
-                    distanceToThisHouse[x+1][y]=distanceToThisHouse[x][y]+1;
-                    queue.add(currentPos);
-                    visitedPlaces.add(currentPos);
-                    sumDistanceToHouses[x+1][y][houseNumber]=distanceToThisHouse[x+1][y];
+                queue.add(currentPos);
+                distanceToThisHouse[x+1][y]=distanceToThisHouse[x][y]+1;
+                if (houseNumber==0 || sumDistanceToHouses[x+1][y]!=0){
+                    sumDistanceToHouses[x+1][y]+=distanceToThisHouse[x+1][y];
+                    numReachablePoints++;
                 }
             }
-            if (x-1>=0 && grid[x-1][y]==0){
+            if (x-1>=0 && grid[x-1][y]==0 && distanceToThisHouse[x-1][y]==0){
                 Map.Entry<Integer,Integer>currentPos=new AbstractMap.SimpleEntry(x-1,y);
-                if (!visitedPlaces.contains(currentPos)){
-                    distanceToThisHouse[x-1][y]=distanceToThisHouse[x][y]+1;
-                    queue.add(currentPos);
-                    visitedPlaces.add(currentPos);
-                    sumDistanceToHouses[x-1][y][houseNumber]=distanceToThisHouse[x-1][y];
+                queue.add(currentPos);
+                distanceToThisHouse[x-1][y]=distanceToThisHouse[x][y]+1;
+                if (houseNumber==0 || sumDistanceToHouses[x-1][y]!=0){
+                    sumDistanceToHouses[x-1][y]+=distanceToThisHouse[x-1][y];
+                    numReachablePoints++;
                 }
 
             }
-            if (y+1<grid[0].length && grid[x][y+1]==0){
+            if (y+1<grid[0].length && grid[x][y+1]==0 && distanceToThisHouse[x][y+1]==0){
                 Map.Entry<Integer,Integer>currentPos=new AbstractMap.SimpleEntry(x,y+1);
-                if (!visitedPlaces.contains(currentPos)){
-                    distanceToThisHouse[x][y+1]=distanceToThisHouse[x][y]+1;
-                    queue.add(currentPos);
-                    visitedPlaces.add(currentPos);
-                    sumDistanceToHouses[x][y+1][houseNumber]=distanceToThisHouse[x][y+1];
+                queue.add(currentPos);
+                distanceToThisHouse[x][y+1]=distanceToThisHouse[x][y]+1;
+                if (houseNumber==0 || sumDistanceToHouses[x][y+1]!=0){
+                    sumDistanceToHouses[x][y+1]+=distanceToThisHouse[x][y+1];
+                    numReachablePoints++;
                 }
 
             }
-            if (y-1>=0 && grid[x][y-1]==0){
+            if (y-1>=0 && grid[x][y-1]==0 && distanceToThisHouse[x][y-1]==0){
                 Map.Entry<Integer,Integer>currentPos=new AbstractMap.SimpleEntry(x,y-1);
-                if (!visitedPlaces.contains(currentPos)){
-                    distanceToThisHouse[x][y-1]=distanceToThisHouse[x][y]+1;
-                    queue.add(currentPos);
-                    visitedPlaces.add(currentPos);
-                    sumDistanceToHouses[x][y-1][houseNumber]=distanceToThisHouse[x][y-1];
+                queue.add(currentPos);
+                distanceToThisHouse[x][y-1]=distanceToThisHouse[x][y]+1;
+                if (houseNumber==0 || sumDistanceToHouses[x][y-1]!=0){
+                    sumDistanceToHouses[x][y-1]+=distanceToThisHouse[x][y-1];
+                    numReachablePoints++;
                 }
             }
         }
+        if (numReachablePoints==0){
+            houseNotReachable=true;
+            return;
+        }
+
+        for (int i=0;i<grid.length;i++){
+            for (int j=0;j<grid[0].length;j++){
+                if (distanceToThisHouse[i][j]==0 && sumDistanceToHouses[i][j]!=0) sumDistanceToHouses[i][j]=0;
+            }
+        }
+
+
     }
     public static void main(String[] args){
         ShortestDistanceFromAllBuildings sdfab=new ShortestDistanceFromAllBuildings();
-        int [][] test1=new int[3][];
-        int [] test10={1,0,2,0,1};
-        int [] test11={0,0,0,0,0};
-        int [] test12={0,0,1,0,0};
+        int [][] test1=new int[5][];
+        int [] test10={0,2,0,2,2,0,2,2};
+        int [] test11={0,2,2,2,1,0,1,2};
+        int [] test12={0,0,0,1,0,2,0,0};
+        int [] test13={2,0,0,2,0,2,2,0};
+        int [] test14={0,0,0,2,0,0,0,0};
         test1[0]=test10;
         test1[1]=test11;
         test1[2]=test12;
-
+        test1[3]=test13;
+        test1[4]=test14;
 
         System.out.println(sdfab.shortestDistance(test1));
     }
